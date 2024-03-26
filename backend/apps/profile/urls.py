dc2aadffe7c3a1da54d7773d3d11a4a4849e9f0b
raparsalan00 <raparsalan00@gmail.com>
2024-03-26 09:07:51 +0700

    push profile

--- backend/apps/profile/urls.py
+++ backend/apps/profile/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", ProfileListCreateAPIView.as_view(), name="profile-list"),
     path("user/", UserListCreateAPIView.as_view(), name="profile-list"),
     path("create/", ProfileListCreateAPIView.as_view(), name="profile-create"),
-    path("details/", ProfileRetrieveUpdateDestroyAPIView.as_view(), name="profile-detail"),
+    path("details/<str:pk>/", ProfileRetrieveUpdateDestroyAPIView.as_view(), name="profile-detail"),
     path("update/<str:pk>/", ProfileRetrieveUpdateDestroyAPIView.as_view(), name="profile-update"),
     path("delete/<str:pk>/", ProfileRetrieveUpdateDestroyAPIView.as_view(), name="profile-delete"),
 ]
