f3a3b322cf22e5aaba397f6f70fff23d372bdf1b
raparsalan00 <raparsalan00@gmail.com>
2024-05-08 15:35:00 +0700

    update seed

--- backend/apps/profile/urls.py
+++ backend/apps/profile/urls.py
@@ -3,8 +3,6 @@ from apps.profile.views.profile_views import *
 
 app_name = "profile"
 urlpatterns = [
-    path("", ProfileAPIView.as_view(), name="my-profile"),
-    path("update/", ProfileUpdateAPIView.as_view(), name="profile-update"),
     path("", ProfileListCreateAPIView.as_view(), name="profile-list"), # bisa filter by id_user
     path("create/", ProfileListCreateAPIView.as_view(), name="profile-create"), # bisa filter by id_user
     path("update/<user_id>/", ProfileUpdateRetrieveAPIView.as_view(), name="profile-update")
--- backend/apps/surat/models.py
+++ backend/apps/surat/models.py
@@ -8,28 +8,19 @@ class Surat(models.Model):
     penerima = models.ManyToManyField(User, related_name='penerima_surat')
     penyetuju = models.ManyToManyField(User, related_name='penyetuju_surat')
     pembuat = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pembuat_surat')
-    # sequence = models.PositiveIntegerField()
     no_agenda = models.CharField(max_length=100)
     no_surat = models.CharField(max_length=100)
     perihal = models.CharField(max_length=100)
-<<<<<<< HEAD
-    status = models.CharField(max_length=100)
     kategori = models.CharField(max_length=100)
     urgensi = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
     tanggal_pengiriman = models.DateField()
-=======
     jenis_surat = models.CharField(max_length=100,)
-    prioritas_surat = models.CharField(max_length=100)
     komentar_surat = models.JSONField(default=list)
     isi_surat = models.TextField(default='-')
-    file_surat = models.FileField(upload_to='event_files/')
     tembusan = models.ManyToManyField(User, related_name='tembusan_surat')
     referensi = models.ForeignKey(Lampiran, on_delete=models.CASCADE, related_name='referensi_surat')
->>>>>>> 90b45eece67d9ccb9bc6b2f51443cc40df3aae5d
-    lampiran = models.ForeignKey(Lampiran, on_delete=models.CASCADE, related_name='lampiran_surat')
-    status = models.CharField(max_length=100)
-    urgensi = models.CharField(max_length=100)  
-    tanggal_pengiriman = models.DateField()  
+    lampiran = models.ForeignKey(Lampiran, on_delete=models.CASCADE, related_name='lampiran_surat') 
     log = models.JSONField(default=list)
     file_surat = models.FileField(upload_to='event_files/')
 
@@ -39,15 +30,6 @@ class Surat(models.Model):
 
     def is_approved(self):
         return self.approvals.count() == self.approver.count()
-
-    def save(self, *args, **kwargs):
-        if not self.sequence:
-            last_surat = Surat.objects.order_by('-sequence').first()
-            if last_surat:
-                self.sequence = last_surat.sequence + 1
-            else:
-                self.sequence = 1
-        super().save(*args, **kwargs)
 
 class Disposisi(models.Model): 
     disposisi_oleh = models.ForeignKey(User, on_delete=models.CASCADE, related_name='disposisi_oleh')
